a4afed697061be97b491c32cded4adc4718b4982
Jesse Brink <jbrink0925@gmail.com>
2022-01-06 17:07:04 -0500

    add tests, more to come

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,78 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { LetterGameProvider } from './useUpdateLetters';
+import Letter from './Components/Letter';
 import App from './App';
+import GameContainer from "./Components/GameContainer";
+import React from 'react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeEach(() => {
+  jest.restoreAllMocks();
+  jest.spyOn(window.HTMLMediaElement.prototype, "play")
+      .mockImplementation(() => {});
+})
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+afterAll(() => {
+  jest.clearAllMocks();
+});
+
+
+describe("Rendering the Letter Component", () => {
+  it("should render the Letter component", () => {
+    const updateParent = jest.fn(() => ({ letters:["A", "E", "I", "O", "U"], correctLetter: "A" }));
+    render(
+      <Letter letter="G" color="purple" updateParent={updateParent} />
+    );
+    
+    const letter = screen.getByText("G")
+    expect(letter).toHaveStyle("color: purple;");
+    expect(letter).toBeInTheDocument();
+  });
+});
+
+describe("Rendering the App Component", () => {
+  it("should render the App component", () => {
+    render(
+      <LetterGameProvider>
+        <App />
+      </LetterGameProvider>
+    );
+  });
+
+  it("should update the App component when the start button is clicked", async() => {
+    const value = {
+      letters: ["A", "E", "I", "O", "U"],
+      correctLetter: "E",
+      numberOfLettersInDOM: 5,
+      isStarted: true
+    };
+    render(
+      <LetterGameProvider value={value}>
+        <App />
+      </LetterGameProvider>
+    );
+
+    const startButton = screen.getByText("Start");
+
+    fireEvent.click(startButton)
+    await waitFor(() => {
+      expect(startButton).toBeDisabled();
+    });
+    screen.debug();
+    const allLetters = screen.getAllByTestId("letter");
+    expect(allLetters.length).toBe(5);
+  });
+});
+
+describe("Rendering the GameContainer Component", () => {
+  it("should render the GameContainer component", () => {
+    render(
+      <LetterGameProvider>
+        <GameContainer />
+      </LetterGameProvider>
+    )
+  });
 });
